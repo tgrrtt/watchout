2f02b30e00ca56fb111243840e0a54cb9453c5f0
tgrrtt <jtgarrett123@gmail.com>
2014-09-19 15:02:55 -0700

    Added board

--- watchout.js
+++ watchout.js
@@ -1,2 +1,26 @@
 // start slingin' some d3 here.
 
+var gameOptions = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20
+}
+
+
+/* Yet to use*/
+var gameStats = {
+  score: 0,
+  bestScore: 0
+}
+
+// axes to set the size of the board and the number of pixels
+var axes = {
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+}
+
+var gameBoard = d3.select('.container')
+    .append('svg:svg')
+    .attr('width', gameOptions.width)
+    .attr('height', gameOptions.height)
